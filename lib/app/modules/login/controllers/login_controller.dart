b92158d653410085ce6d19e65ca7e6db46e3bbf6
Syafrial Zulmi <syafrialzulmi@gmail.com>
2022-05-15 13:36:02 +0700

    04 Auth Login

---
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_presence4/app/routes/app_pages.dart';
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  TextEditingController emailC = TextEditingController();
+  TextEditingController passC = TextEditingController();
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  void login() async {
+    if (emailC.text.isNotEmpty && passC.text.isNotEmpty) {
+      try {
+        UserCredential userCredential = await auth.signInWithEmailAndPassword(
+            email: emailC.text, password: passC.text);
+
+        Get.offAllNamed(Routes.HOME);
+        if (userCredential.user != null) {
+          if (userCredential.user!.emailVerified == true) {
+            Get.offAllNamed(Routes.HOME);
+          }
+        } else {
+          Get.defaultDialog(
+            title: "Email belum Verifikasi",
+            middleText:
+                "Kamu belum verifikasi akun ini. Lakukan verifikasi di akun email anda.",
+          );
+        }
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'user-not-found') {
+          Get.snackbar("Terjadi Kesalahan", "Pengguna email tidak terdaftar.");
+        } else if (e.code == 'wrong-password') {
+          Get.snackbar(
+              "Terjadi Kesalahan", "Password yang anda masukkan salah.");
+        }
+      } catch (e) {
+        Get.snackbar("Terjadi Kesalahan", "Tidak dapat login ");
+      }
+    } else {
+      Get.snackbar("Terjadi Kesalahan", "Email dan Password wajib diisi.");
+    }
+  }
+}
---
+++ lib/app/modules/login/views/login_view.dart
@@ -0,0 +1,55 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/login_controller.dart';
+
+class LoginView extends GetView<LoginController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('LOGIN'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          TextField(
+            controller: controller.emailC,
+            decoration: InputDecoration(
+              labelText: "Email",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextField(
+            controller: controller.passC,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: "Password",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('LOGIN'),
+          ),
+          TextButton(
+            onPressed: () {
+              controller.login();
+            },
+            child: Text('Lupa Password ?'),
+          )
+        ],
+      ),
+    );
+  }
+}
